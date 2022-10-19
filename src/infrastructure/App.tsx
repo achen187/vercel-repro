941b33295b632d1ef90f892e77c23e82dbc40774
achen187 <chen.andy1993@gmail.com>
2022-10-19 18:26:19 -0400

    minimal build

---
+++ src/infrastructure/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+type AppProps = {};
+
+const App: React.FC<AppProps> = (props: AppProps) => {
+  return <div>Hello world</div>;
+};
+
+export default App;
